59ecbbbe246fd79f6a21009b3ca33674cf0a7836
muzykadenys <denysmuzyka@gmail.com>
2024-05-20 22:21:08 +0300

    add loader

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Cursova MD AM-33</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
